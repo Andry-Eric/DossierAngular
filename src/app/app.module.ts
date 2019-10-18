7dc56a0c526206b71aa0f2b18a40a218ca5e48a3
Eric <andryericrandrianasolo@gmail.com>
2019-10-18 16:59:39 +0300

    Page 404 et autorisation des pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,24 @@ import { AppareilViewComponent } from "./appareil-view/appareil-view.component";
 
 import { RouterModule, Routes } from "@angular/router";
 import { AuthService } from "./services/auth.service";
-import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { SingleAppareilComponent } from "./single-appareil/single-appareil.component";
+import { FourOhFourComponent } from "./four-oh-four/four-oh-four.component";
+import { AuthGuard } from "./services/auth-guard.service";
 
 const appRoutes: Routes = [
-  { path: "appareils", component: AppareilViewComponent },
+  {
+    path: "appareils",
+    canActivate: [AuthGuard],
+    component: AppareilViewComponent
+  },
+  {
+    path: "appareils/:id",
+    canActivate: [AuthGuard],
+    component: SingleAppareilComponent
+  },
   { path: "auth", component: AuthComponent },
-  { path: "", component: AppareilViewComponent }
+  { path: "not-found", component: FourOhFourComponent }, // mettez à la fin de ligne
+  { path: "**", redirectTo: "/not-found" }
 ];
 
 @NgModule({
@@ -27,11 +39,12 @@ const appRoutes: Routes = [
     AppareilComponent,
     AuthComponent,
     AppareilViewComponent,
-    SingleAppareilComponent
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   //  importer routes dans imports
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [AppareilService, AuthService],
+  providers: [AppareilService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
